b40a70671245c93212b1934b8faebd89dba16664
Xavier Munroe <munroexavier@gmail.com>
2021-01-23 18:18:10 -0500

    starting main nav

--- components/nav/MainNav.tsx
+++ components/nav/MainNav.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "../../styles/MainNav.module.scss";
-import { useMediaQuery } from "react-responsive";
 
 function MainNav() {
-  const isDesktop = useMediaQuery({ query: "(min-device-width: 762px)" });
-  const isMobile = useMediaQuery({ query: "(max-device-width: 761px)" });
 
   return (
-    <React.Fragment>
-      {isDesktop && <div></div>}
-      {isMobile && <div></div>}
-    </React.Fragment>
+    <div className={styles.container}>
+      <nav className={styles.navDesktop}></nav>
+      <nav className={styles.navMobile}></nav>
+    </div>
   );
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import "regenerator-runtime";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/globals.scss";
 import AuthProvider from "../context/authContext";
+import MainNav from "../components/nav/MainNav";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <MainNav />
       <Component {...pageProps} />
     </AuthProvider>
   );
